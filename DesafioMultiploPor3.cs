60785b06bca23ddd23879b4a1ab74223c4abe161
Luccas Oliveira <102065560+luccasfoliveira@users.noreply.github.com>
2023-02-09 02:06:34 -0300

    Create DesafioMultiploPor3.cs

---
+++ DesafioMultiploPor3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args) {
+        Console.WriteLine("Desafio 16 - Multiplo de 03");
+        Console.WriteLine("Imprimir os multiplos de 3");
+
+        for (int i = 3; i <= 100; i += 3) {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine(("Tecle Enter para sair..."));
+        Console.ReadLine();
+    }
+}
