ea3e133cf8685a70d80d6078081c33574cf79c49
Luccas Oliveira <102065560+luccasfoliveira@users.noreply.github.com>
2023-02-09 02:28:57 -0300

    Create DesafioFatorial.cs

---
+++ DesafioFatorial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args) {
+        Console.WriteLine("Projeto 17 - Desafio Fatorial (01-10):");
+            
+        for (int i = 1; i <= 10; i++) {
+            int fatorial = 1;
+            for (int j = 1;j < i+1; j++) {
+                fatorial *= j;
+            }
+            Console.WriteLine(fatorial);
+        }
+        Console.WriteLine("Tecle Enter para sair...");
+        Console.ReadLine();
+    }
+}
