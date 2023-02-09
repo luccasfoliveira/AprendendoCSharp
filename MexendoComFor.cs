b172722a450e9ee7c0c7a042c747b7dc3d3a498c
Luccas Oliveira <102065560+luccasfoliveira@users.noreply.github.com>
2023-02-09 01:58:11 -0300

    Create MexendoComFor.cs

---
+++ MexendoComFor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args) {
+        Console.WriteLine("Projeto 15 - Mexnedo com o For:");
+        //*
+        //**
+        //***
+        //****
+        //*****
+        //******
+        //*******
+        //********
+        //*********
+        //**********
+
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 0; j < i; j++) {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
